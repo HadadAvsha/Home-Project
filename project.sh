0237268d6f449afa5414804147702daa13019f56
Avshalom Hadad <hadadavsha@gmail.com>
2023-01-21 18:52:12 +0200

    separated ci/cd

--- project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pushd ~/Desktop/Home-Project/terraform
-terraform apply -auto-approve
-cd ../ansible
-bash run.sh
-sleep 120
-helm install argo-cd -f ../terraform/argo-cd-values.yaml argo/argo-cd --version 5.8.3
-sleep 60
-bash ~/my_scripts/argo-repo_main-app.sh
