42ac9d8707f34594c8c0b7fd4d545187354bfb2f
Avshalom Hadad <hadadavsha@gmail.com>
2023-01-22 13:10:31 +0200

    added readme, argocd ns

--- terraform/argo-cd.tf
+++ terraform/argo-cd.tf
@@ -6,21 +6,13 @@ provider "helm" {
 
 
 resource "helm_release" "argocd" {
-  depends_on = [null_resource.ModifyApplyAnsiblePlayBook]
-
-    # provisioner "local-exec" {
-    #     interpreter = ["/bin/bash", "-c"]
-    #     command = <<-EOT
-    #         bash ~/my_scripts/argo-repo_main-app.sh
-    #     EOT
-    # }
-
-  name       = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "default"
-  version    = "5.13.8"
-  cleanup_on_fail = true
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argo-cd"
+  create_namespace = true
+  version          = "5.13.8"
+  cleanup_on_fail  = true
   
   values = [
     file("argo-cd-values.yaml")
@@ -33,4 +25,6 @@ resource "helm_release" "argocd" {
         bash ~/my_scripts/argo-repo_main-app.sh
     EOT
   }
+
+  depends_on = [null_resource.ModifyApplyAnsiblePlayBook]
 }
